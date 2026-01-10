be7b4530e86bebee2ee7e02bd2da5724d884b0bc
Yaswanth <yaswanth.004.001@gmail.com>
2026-01-10 19:07:04 +0530

    Add Task JPA entity

---
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -0,0 +1,75 @@
+package com.example.taskmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(length = 500)
+	private String decription;
+	
+	@Column(nullable = false)
+	private boolean complpeted;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDecription() {
+		return decription;
+	}
+
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+
+	public boolean isComplpeted() {
+		return complpeted;
+	}
+
+	public void setComplpeted(boolean complpeted) {
+		this.complpeted = complpeted;
+	}
+
+	public Task(Long id, String title, String decription, boolean complpeted) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.decription = decription;
+		this.complpeted = complpeted;
+	}
+
+	public Task() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}

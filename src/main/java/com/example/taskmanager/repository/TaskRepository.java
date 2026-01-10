6bd7e5afe4759150aa561898beb943763f1a6565
Yaswanth <yaswanth.004.001@gmail.com>
2026-01-10 19:16:06 +0530

    Add TaskRepository using Spring Data JPA

---
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.example.taskmanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.taskmanager.model.Task;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
+
+}
